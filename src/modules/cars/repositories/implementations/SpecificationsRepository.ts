e6729fbe8ba78edd861593d9a5566b57a0110dca
Rodrigo Dematei de Freitas <rodfrei@gmail.com>
2021-03-21 11:31:40 -0300

    Initial Commit

---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,47 @@
+import { Specification } from "../../model/Specification";
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationDTO,
+} from "../ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private Specifications: Specification[] = [];
+
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
+    this.Specifications = [];
+  }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+    return SpecificationsRepository.INSTANCE;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.Specifications.push(specification);
+  }
+
+  list(): Specification[] {
+    return this.Specifications;
+  }
+
+  findByName(name: string): Specification {
+    const Specification = this.Specifications.find(
+      (Specification) => Specification.name === name
+    );
+    return Specification;
+  }
+}
+
+export { SpecificationsRepository };
